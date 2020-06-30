f95ae09c08c6c4ef88871fb3d6588c092d870f9c
Luciano Peñafiel <luciano.penafiel@aerolab.co>
2020-06-30 08:00:36 -0300

    feat: poc runting caching config

--- next.config.js
+++ next.config.js
@@ -11,52 +11,36 @@ const getSecret = (key) => {
   return process.env[key];
 };
 
-// module.exports = withPWA({
-//   pwa: {
-//     dest: 'public',
-//     runtimeCaching: [
-//       {
-//         urlPattern: '/',
-//         handler: 'NetworkFirst',
-//         options: {
-//           cacheName: 'html-cache',
-//         },
-//       },
-//       {
-//         urlPattern: '/login',
-//         handler: 'NetworkFirst',
-//         options: {
-//           cacheName: 'html-cache',
-//         },
-//       },
-//       {
-//         urlPattern: /.*\.(?:png|jpg|jpeg|svg|gif)/,
-//         handler: 'CacheFirst',
-//         options: {
-//           cacheName: 'image-cache',
-//           cacheableResponse: {
-//             statuses: [0, 200],
-//           },
-//         },
-//       },
-//     ],
-//   },
-//   serverRuntimeConfig: {
-//     BASE_URL: getSecret('BASE_URL'),
-//   },
-//   publicRuntimeConfig: {
-//     BASE_URL: getSecret('BASE_URL'),
-//     API_KEY: getSecret('API_KEY'),
-//     AUTH_DOMAIN: getSecret('AUTH_DOMAIN'),
-//     DATABASE_URL: getSecret('DATABASE_URL'),
-//     PROJECT_ID: getSecret('PROJECT_ID'),
-//     STORAGE_BUCKET: getSecret('STORAGE_BUCKET'),
-//     MESSAGING_SENDER_ID: getSecret('MESSAGING_SENDER_ID'),
-//     APP_ID: getSecret('APP_ID'),
-//   },
-// });
-
-module.exports = {
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching: [
+      {
+        urlPattern: '/',
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'html-cache',
+        },
+      },
+      {
+        urlPattern: '/[^3]/login//',
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'html-cache',
+        },
+      },
+      {
+        urlPattern: /.*\.(?:png|jpg|jpeg|svg|gif)/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'image-cache',
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
   serverRuntimeConfig: {
     BASE_URL: getSecret('BASE_URL'),
   },
@@ -70,4 +54,20 @@ module.exports = {
     MESSAGING_SENDER_ID: getSecret('MESSAGING_SENDER_ID'),
     APP_ID: getSecret('APP_ID'),
   },
-};
+});
+
+// module.exports = {
+//   serverRuntimeConfig: {
+//     BASE_URL: getSecret('BASE_URL'),
+//   },
+//   publicRuntimeConfig: {
+//     BASE_URL: getSecret('BASE_URL'),
+//     API_KEY: getSecret('API_KEY'),
+//     AUTH_DOMAIN: getSecret('AUTH_DOMAIN'),
+//     DATABASE_URL: getSecret('DATABASE_URL'),
+//     PROJECT_ID: getSecret('PROJECT_ID'),
+//     STORAGE_BUCKET: getSecret('STORAGE_BUCKET'),
+//     MESSAGING_SENDER_ID: getSecret('MESSAGING_SENDER_ID'),
+//     APP_ID: getSecret('APP_ID'),
+//   },
+// };
