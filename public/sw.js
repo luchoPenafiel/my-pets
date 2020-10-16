8e59119c19751c51392966eff91429fdf855f12e
Luciano Penafiel <luciano.penafiel@aerolab.co>
2020-10-15 21:36:04 -0300

    feat: gtm

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "d7fdad38464c5f0939733709f4c2ce07"
+    "revision": "0e73045c7890d856e661ee7ff98ebd78"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
