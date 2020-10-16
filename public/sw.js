0406d8936e5c9120daaccae8e263dc9261ce6c50
Luciano Penafiel <luciano.penafiel@aerolab.co>
2020-10-16 10:10:29 -0300

    feat: vet copy

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "1d43d3e5134b389caf72b0fd7facc1cf"
+    "revision": "b0d8b914d0a342a4a026482c9ffcd1d0"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
