ac9946edcb117385f40685dd22d541df90d712dc
Luciano Penafiel <luciano.penafiel@aerolab.co>
2020-10-15 21:48:58 -0300

    feat: hot fix gtm

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -43,11 +43,11 @@ class MyDocument extends NextDocument {
             async
             dangerouslySetInnerHTML={{
               __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', 'UA-180665375-1');
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','UA-180665375-1');
                     `,
             }}
           />
